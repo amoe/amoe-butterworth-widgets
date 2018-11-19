64c1201a981104979692aa7066c926d183d5c78f
David Banks <amoebae@gmail.com>
2018-11-19 12:52:43 +0000

    using less

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home">
 
-    <p>Foo <airplay-icon/> Bar</p>
+    <p>Foo <circle-icon/> Bar</p>
 
     <div class="widget-container">
       <div class="widget" v-for="widget in widgets">
-        <input type="text" size="32" v-model="widget.value"></input>
+        <input class="taxon-name" type="text" size="32" v-model="widget.value"></input>
+        <div class="level-container">
+          <circle-icon v-for="n in widget.level"
+                       :key="n"></circle-icon>
+        </div>
       </div>
     </div>
   </div>
@@ -14,11 +18,11 @@
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import { AirplayIcon } from 'vue-feather-icons'
+import { CircleIcon } from 'vue-feather-icons'
 
 export default Vue.extend({
     name: 'home',
-    components: { AirplayIcon },
+    components: { CircleIcon },
     data() {
         return {
             widgets: [{
@@ -34,18 +38,30 @@ export default Vue.extend({
 });
 </script>
 
-<style>
-
-
+<style lang="less">
 .widget-container {
     display: flex;
 }
 
 .widget {
-    background-color: green;
+    background-color: hsl(105, 100%, 40%);
     padding-top: 1em;
     padding-bottom: 1em;
     margin-left: 0.2em;
     margin-right: 0.2em;
+    display: flex;
+    flex-direction: column;
+}
+
+.level-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 1em;
+}
+
+.taxon-name {
+    margin-left: 1em;
+    margin-right: 1em;
 }
 </style>
