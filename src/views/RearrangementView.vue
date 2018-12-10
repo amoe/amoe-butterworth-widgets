fd9a306664edf736099d25f2340880c339cd9ea2
David Banks <amoebae@gmail.com>
2018-12-10 18:21:51 +0000

    demonstrate correct typing of refs

--- src/views/RearrangementView.vue
+++ src/views/RearrangementView.vue
@@ -1,13 +1,51 @@
 <template>
-<p>Some text</p>
+<div class="rearrangement-view">
+  <div class="box" v-for="(box, index) in boxes" ref="boxes">
+    {{index}}: {{box}}
+  </div>
+</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue, {VueConstructor} from 'vue';
+import { Draggable } from 'gsap/Draggable';
 
-export default Vue.extend({
+interface RearrangementViewRefs {
+    $refs: {
+        boxes: Element[]
+    }
+};
+
+type AugmentedVue = VueConstructor<Vue & RearrangementViewRefs>;
+
+export default (Vue as AugmentedVue).extend({
+    data() {
+        return {
+            boxes: ['Larry', 'Curly', 'Moe']
+        };
+    },
+    mounted() {
+        this.$nextTick(this.setupDraggables);
+    },
+    methods: {
+        setupDraggables() {
+            console.log("found element as %o", this.$refs.boxes);
+            const foo: Element[] = this.$refs.boxes
+        }
+    }
 });
 </script>
 
 <style lang="less">
+.rearrangement-view {
+    display: flex;
+    flex-direction: row;
+}
+
+.box {
+    height: 200px;
+    width: 100px;
+    background-color: orange;
+    margin: 1em;
+}
 </style>
