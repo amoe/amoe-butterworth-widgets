4fc4f23c7be26ef1d75b71f884c21610e2cb59af
David Banks <amoebae@gmail.com>
2018-12-13 09:46:43 +0000

    restore colour coding by hardcoding taxonomies

--- src/views/Orthodox.vue
+++ src/views/Orthodox.vue
@@ -11,12 +11,42 @@ import WidgetView from '@/components/WidgetView.vue'; // @ is an alias to /src
 import * as log from 'loglevel';
 import {TaxonomyNodeModel} from '@/types';
 
-const TAXONOMY_DATA = {
+const MUSIC_TAXONOMY_JSON = {
     'children': [{'content': 'Rock', 'id': 1, 'label': 'Taxon',
                   'children': [{'content': 'Metal', 'id': 3, 'label': 'Taxon'}]},
                  {'content': 'Classical', 'id': 2, 'label': 'Taxon',
                   'children': [{'content': 'Baroque', 'id': 4, 'label': 'Taxon'}]}],
     'content': 'Music',
+    'id': 0,
+    'label': 'Taxon'
+};
+
+const OCCUPATION_TAXONOMY_JSON = {
+    'children': [{'content': 'Manufacturing', 'id': 1, 'label': 'Taxon',
+                  'children': [
+                      {'content': 'Wood workers',
+                       'id': 2, 
+                       'label': 'Taxon', 
+                       'children': [
+                           {'content': 'Bandbox-maker',
+                            'id': 3,
+                            'label': 'Taxon'}
+                       ]}
+                  ]}],
+
+    'content': 'Occupation',
+    'id': 0,
+    'label': 'Taxon'
+};
+
+const PLACE_TAXONOMY_JSON = {
+    'children': [{'content': 'Country', 'id': 1, 'label': 'Taxon',
+                  'children': [
+                      {'content': 'France',
+                       'id': 2, 
+                       'label': 'Taxon'}]
+                 }],
+    'content': 'Place',
     'id': 0,
     'label': 'Taxon'
 };
@@ -32,7 +62,11 @@ export default Vue.extend({
         return {
             someArray: [1, 2, 3],
             anotherArray: [4, 5, 6],
-            taxonomies: {'Music': t.parse<TaxonomyNodeModel>(TAXONOMY_DATA)}
+            taxonomies: {
+                'Music': t.parse<TaxonomyNodeModel>(MUSIC_TAXONOMY_JSON),
+                'Occupation': t.parse<TaxonomyNodeModel>(OCCUPATION_TAXONOMY_JSON),
+                'Place': t.parse<TaxonomyNodeModel>(PLACE_TAXONOMY_JSON)
+            }
         }
     },
     mounted() {
