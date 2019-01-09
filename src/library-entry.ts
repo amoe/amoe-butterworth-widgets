fd2ff6cbc927e67c993511bb48939150cb326ed8
David Banks <amoebae@gmail.com>
2019-01-09 11:16:34 +0000

    attempting to export component type

--- src/library-entry.ts
+++ src/library-entry.ts
@@ -2,4 +2,8 @@ import Vue from 'vue';
 import WidgetView from './components/WidgetView.vue';
 import WidgetsModule from '@/vuex-module';
 
+export interface WidgetViewComponent {
+    getQuery(): any;
+}
+
 export { WidgetView, WidgetsModule }
